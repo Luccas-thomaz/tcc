29e85c79246ec505519d5c1f0d0d43c02ec51e06
Luccas Thomaz Pereira Lavrador <108094478+Luccas-thomaz@users.noreply.github.com>
2025-10-11 21:16:39 -0300

    jogo criado v1

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "quiz-cache-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "correct.mp3",
+  "wrong.mp3",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Instalação
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Ativação
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Busca offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
